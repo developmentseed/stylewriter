f46aac52e8991d99b650bf45dac3e0e4daa188ae
Tom MacWright <macwright@gmail.com>
2010-06-11 17:33:36 -0400

    Dual-purpose view now, and theme files

---
+++ views/stylewriter-legend.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id$
+
+/**
+ * @param $swatches string
+ */
+?>
+<div class='stylewriter-legend'>
+  <?php echo $swatches; ?>
+</div>
---
+++ views/stylewriter-swatch.tpl.php
@@ -0,0 +1,15 @@
+<?php
+// $Id$
+
+/**
+ * @param $color
+ * @param $value
+ */
+?>
+<div class='stylewriter-swatch'>
+  <div class='stylewriter-swatch-color' 
+    style='background-color: <?php echo $color; ?>'>&nbsp;</div>
+  <span class='stylewriter-swatch-value'>
+    <?php echo $value; ?>
+  </span>
+</div>
