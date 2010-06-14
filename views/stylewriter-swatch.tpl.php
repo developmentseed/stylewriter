928978f10935b8918fbb4d95c410a8b9a6d9e1c9
Tom MacWright <macwright@gmail.com>
2010-06-14 16:42:12 -0400

    Adding nice facet-based cache clearing system to stylewriter - without a current backend and needs style facet control

--- views/stylewriter-swatch.tpl.php
+++ views/stylewriter-swatch.tpl.php
@@ -4,10 +4,11 @@
 /**
  * @param $color
  * @param $value
+ * @param $place
  */
 ?>
 <div class='stylewriter-swatch'>
-  <div class='stylewriter-swatch-color' 
+<div class='stylewriter-swatch-color stylewriter-swatch-place-<?php echo $place; ?>' 
     style='background-color: <?php echo $color; ?>'>&nbsp;</div>
   <span class='stylewriter-swatch-value'>
     <?php echo $value; ?>
