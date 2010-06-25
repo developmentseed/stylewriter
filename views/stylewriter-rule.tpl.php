47d96fedd6bcd7b51e088631ddd7dfa326f3debb
Young Hahn <yfhahn@gmail.com>
2010-06-25 13:35:28 -0400

    Add opacity option to style plugin.

--- views/stylewriter-rule.tpl.php
+++ views/stylewriter-rule.tpl.php
@@ -13,6 +13,6 @@
   <Filter>[<?php echo $join_field_name; ?>] = '<?php echo $join_field_value; ?>'</Filter>
   <PolygonSymbolizer>
     <CssParameter name="fill"><?php echo $color; ?></CssParameter>
-    <CssParameter name="fill-opacity">0.4</CssParameter>
+    <CssParameter name="fill-opacity"><?php echo $opacity; ?></CssParameter>
   </PolygonSymbolizer>
 </Rule>
