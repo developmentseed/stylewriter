a16a53223f1c395dab63de7c8016c5987d217140
Tom MacWright <macwright@gmail.com>
2010-06-30 16:28:36 -0400

    Adding stroke option and output in mapfiles

--- views/stylewriter-rule.tpl.php
+++ views/stylewriter-rule.tpl.php
@@ -7,6 +7,9 @@
  * @param $join_field_value
  *
  * @param $color
+ * @param $opacity
+ *
+ * @param $stroke
  */
 ?>
 <Rule>
@@ -15,4 +18,11 @@
     <CssParameter name="fill"><?php echo $color; ?></CssParameter>
     <CssParameter name="fill-opacity"><?php echo $opacity; ?></CssParameter>
   </PolygonSymbolizer>
+  <?php if ($stroke['enable'] == '1'): ?>
+  <LineSymbolizer>
+    <CssParameter name="stroke"><?php echo         $stroke['color']; ?></CssParameter>
+    <CssParameter name="stroke-width"><?php echo   $stroke['width']; ?></CssParameter>
+    <CssParameter name="stroke-opacity"><?php echo $stroke['opacity']; ?></CssParameter>
+  </LineSymbolizer>
+  <?php endif; ?>
 </Rule>
