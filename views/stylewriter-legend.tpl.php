32de0ab432d8c20ed031c33f784f17f0970ac0a8
Tom MacWright <macwright@gmail.com>
2010-07-09 13:20:41 -0400

    Adding label to stylewriter legends

--- views/stylewriter-legend.tpl.php
+++ views/stylewriter-legend.tpl.php
@@ -2,12 +2,14 @@
 // $Id$
 
 /**
+ * @param $label string
  * @param $swatches string
  * @param $value_min string
  * @param $value_max string
  */
 ?>
 <div class='stylewriter-legend'>
+  <div class='stylewriter-label'><?php echo $label ?></div>
   <div class='stylewriter-value'><?php echo $value_min ?></div>
   <div class='stylewriter-swatches'><?php echo $swatches; ?></div>
   <div class='stylewriter-value'><?php echo $value_max ?></div>
