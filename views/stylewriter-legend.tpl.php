bec61dd8ddc4c5078c3c7ecfa3fd35239dbdc732
Tom MacWright <macwright@gmail.com>
2010-09-03 16:15:07 -0400

    Fixing fail for views with many displays

--- views/stylewriter-legend.tpl.php
+++ views/stylewriter-legend.tpl.php
@@ -8,9 +8,9 @@
  * @param $value_max string
  */
 ?>
-<div class='stylewriter-legend'>
+<div class='stylewriter-legend clear-block'>
   <div class='stylewriter-label'><?php echo $label ?></div>
   <div class='stylewriter-value'><?php echo $value_min ?></div>
-  <div class='stylewriter-swatches'><?php echo $swatches; ?></div>
+  <div class='stylewriter-swatches clear-block'><?php echo $swatches; ?></div>
   <div class='stylewriter-value'><?php echo $value_max ?></div>
 </div>
