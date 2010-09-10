bb622c6b4e39c8464fbe2926b4b37905fc4829f8
Tom MacWright <macwright@gmail.com>
2010-09-10 13:07:28 -0400

    Adding new point legends

---
+++ views/stylewriter-point-swatch.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id$
+
+/**
+ * @param $color
+ * @param $dist
+ */
+?>
+<div class='stylewriter-swatch stylewriter-swatch-point clear-block'>
+  <div class='stylewriter-swatch-point-sym stylewriter-swatch-color-dist-<?php echo $dist; ?> sym-<?php echo $color; ?>'></div>
+  <div class='stylewriter-swatch-label'><?php echo $value_min; ?> - <?php echo $value_max; ?></div>
+</div>
