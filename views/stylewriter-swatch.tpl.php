79158f7595b534cfe0f2405fb0345202fd3d71e9
Tom MacWright <macwright@gmail.com>
2010-09-10 10:32:16 -0400

    New-style legends

--- views/stylewriter-swatch.tpl.php
+++ views/stylewriter-swatch.tpl.php
@@ -2,10 +2,11 @@
 // $Id$
 
 /**
- * @param $value
+ * @param $color
  * @param $dist
  */
 ?>
-<div class='stylewriter-swatch'>
+<div class='stylewriter-swatch clear-block'>
   <div class='stylewriter-swatch-color stylewriter-swatch-color-dist-<?php echo $dist; ?>' style='background-color:<?php echo $color; ?>'></div>
+  <div class='stylewriter-swatch-label'><?php echo $value_min; ?> - <?php echo $value_max; ?></div>
 </div>
