e77cb4211d3d30da491ca2d38c2f6a37a6911fe4
Tom MacWright <macwright@gmail.com>
2010-09-28 14:31:30 -0400

    Better check for MetaWriter existence

--- views/stylewriter-document.tpl.php
+++ views/stylewriter-document.tpl.php
@@ -11,7 +11,7 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <!DOCTYPE Map>
 <Map bgcolor="transparent" srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over">
-  <?php if (isset($metawriter)): ?>
+  <?php if (!empty($metawriter['name'])): ?>
     <MetaWriter 
         name="<?php echo $metawriter['name']; ?>" 
         only-nonempty="false"
